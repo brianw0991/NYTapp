662ede769d215e1c16f6d61815d4df3bbea99683
Grace Miller <gracemiller@Graces-MacBook-Pro-2.local>
2018-04-27 10:24:33 -0500

    debugging and closing function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,71 +7,73 @@
 
 //function to generate buttons that add pagination text to the 
 //initial api query if results request is more than 10
-
+displayNYTResults();
 
 
 function displayNYTResults() {
 
     //example: var movie = $(this).attr("data-name");
     var searchTerm = $("#searchterm").text();
-    var recordstoRetrieve =  $("#retrieve").text();
+    var recordstoRetrieve = $("#retrieve").text();
     var startYear = $("startyear").text();
     var endYear = $("endyear").text();
 
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-  'api-key': "3b0912289d5d4dfb9f98296b8558eb8a",
-  'q': "trump",
-  'begin_date': startYear + "0101",
-  'end_date': endYear + "0101"
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
+    url += '?' + $.param({
+        'api-key': "3b0912289d5d4dfb9f98296b8558eb8a",
+        'q': "trump",
+        'begin_date': startYear + "0101",
+        'end_date': endYear + "0101"
+    });
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function (result) {
 
-$("#search").on(click, function(){
-    console.log(result);
-    generateResultDivs();
-});
-    $("div.content").append(resultDiv)
-}).fail(function(err) {
-  throw err;
-});
-
-
-function multiplePageResults (numberofPages){
-    var   numberofResults = userINput / 10;
-for (i=0 ; i<numberofResults.length; i++){
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-    'api-key': "3b0912289d5d4dfb9f98296b8558eb8a",
-    'q': "trump",
-    'begin_date': startYear + "0101",
-    'end_date': endYear + "0101",
-  'page': page[i]
-});
-
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});
+        $("#search").on(click, function () {
+            console.log(result);
+            generateResultDivs();
+        });
+        $("div.content").append(resultDiv)
+    }).fail(function (err) {
+        throw err;
+    });
+    
 }
 
-function generateResultDivs(){
-   
-    for (i = 0; i < 10; i++){
+function generateResultDivs() {
+
+    for (i = 0; i < 10; i++) {
         var target = results.response.docs[i];
         var resultTitle = $("<h2>").text(target.headline.main);
         var resultAuthor = $("<p>").text(target.byline.original);
         var resultLink = $("<a>").attr("href", target.web_url);
         var resultDiv = $("<div>").attr("class", "result-entry");
-       
+
         $(resultDiv).append(resultNumber[i], resultTitle, resultAuthor);
+    };
+    $(content).append(resultDiv);
+}
+
+function multiplePageResults(numberofPages) {
+    var numberofResults = userINput / 10;
+    for (i = 0; i < numberofResults.length; i++) {
+        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        url += '?' + $.param({
+            'api-key': "3b0912289d5d4dfb9f98296b8558eb8a",
+            'q': "trump",
+            'begin_date': startYear + "0101",
+            'end_date': endYear + "0101",
+            'page': page[i]
+        });
+
+        $.ajax({
+            url: url,
+            method: 'GET',
+        }).done(function (result) {
+            console.log(result);
+        }).fail(function (err) {
+            throw err;
+        });
     }
-    $(content).append(resultDiv); 
-};
+}
